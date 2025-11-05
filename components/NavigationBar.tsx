22ae075eca09e3e1cf8d3ca0cd8a72e11f01f64a
Sans <167203085+Sansalmao@users.noreply.github.com>
2025-11-04 23:23:38 -0400

    Update NavigationBar.tsx

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,30 +1,35 @@
 import React from "react";
 import { useRouter, usePathname } from "expo-router";
 import styled from "styled-components/native";
+import { Dimensions } from "react-native";
 
-// Importar los íconos en sus versiones outline y filled
-const homeIconOutline = require("../assets/images/homeIcon_outline.png");
-const homeIconFilled = require("../assets/images/homeIcon_filled.png");
-const searchIconOutline = require("../assets/images/searchIcon_outline.png");
-const notificationIconOutline = require("../assets/images/notificationIcon_outline.png");
-const notificationIconFilled = require("../assets/images/notificationIcon_filled.png");
-const messagesIconOutline = require("../assets/images/messagesIcon_outline.png");
-const messagesIconFilled = require("../assets/images/messagesIcon_filled.png");
+const homeIconOutline = require("../images/homeIcon_outline.png");
+const homeIconFilled = require("../images/homeIcon_filled.png");
+const searchIconOutline = require("../images/searchIcon_outline.png");
+const notificationIconOutline = require("../images/notificationIcon_outline.png");
+const notificationIconFilled = require("../images/notificationIcon_filled.png");
+const messagesIconOutline = require("../images/messagesIcon_outline.png");
+const messagesIconFilled = require("../images/messagesIcon_filled.png");
+
+const { width: screenWidth } = Dimensions.get("window");
 
 interface NavigationBarProps {
   style?: any;
 }
+
+const NAV_BAR_WIDTH = 180;
+const NAV_BAR_HEIGHT = 87;
 
 export const NavigationBar = ({ style }: NavigationBarProps) => {
   const router = useRouter();
   const pathname = usePathname();
 
   const navigateTo = (screen: string) => {
-    router.push(`/screens/${screen}`);
+    router.push(`/(tabs)/${screen}`);
   };
 
   const getIconSource = (screen: string) => {
-    const isActive = pathname === `/screens/${screen}`;
+    const isActive = pathname === `/(tabs)/${screen}`;
 
     switch (screen) {
       case "home":
@@ -42,17 +47,18 @@ export const NavigationBar = ({ style }: NavigationBarProps) => {
 
   return (
     <Container style={style}>
-      {" "}
-      {/* Aplicar estilos adicionales */}
       <NavButton onPress={() => navigateTo("home")}>
         <Icon source={getIconSource("home")} />
       </NavButton>
+
       <NavButton onPress={() => navigateTo("search")}>
         <Icon source={getIconSource("search")} />
       </NavButton>
+
       <NavButton onPress={() => navigateTo("notifications")}>
         <Icon source={getIconSource("notifications")} />
       </NavButton>
+
       <NavButton onPress={() => navigateTo("messages")}>
         <Icon source={getIconSource("messages")} />
       </NavButton>
@@ -60,10 +66,9 @@ export const NavigationBar = ({ style }: NavigationBarProps) => {
   );
 };
 
-// Styled Components
 const Container = styled.View`
-  width: 142px;
-  height: 87px;
+  width: ${NAV_BAR_WIDTH}px;
+  height: ${NAV_BAR_HEIGHT}px;
   background-color: #423646;
   border-radius: 12px;
   flex-direction: row;
