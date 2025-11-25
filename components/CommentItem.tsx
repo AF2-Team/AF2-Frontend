5a961f080644336ccb2c7b751072738988e137a8
Sans <167203085+Sansalmao@users.noreply.github.com>
2025-11-25 16:58:11 -0400

    Update CommentItem.tsx

--- components/CommentItem.tsx
+++ components/CommentItem.tsx
@@ -1,4 +1,3 @@
-// components/CommentItem.tsx
 import React from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
